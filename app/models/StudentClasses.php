a0789519b108c03ecf9d657b4c91318ff16c60cb
harsha-headrun <harsha@notemonk.com>
2017-09-01 08:18:55 -0400

    enrolled kids not present in enrolled list

--- app/models/StudentClasses.php
+++ app/models/StudentClasses.php
@@ -35,7 +35,7 @@ class StudentClasses extends \Eloquent {
                         "SELECT * from students where id IN (SELECT distinct (student_classes.student_id)
                          FROM student_classes where  
                           enrollment_end_date >='".$present_date->toDateString()."' AND student_classes.status 
-                         IN ('enrolled')) and students.franchisee_id='".$franchiseeId."'")
+                         IN ('enrolled', 'transferred_to_other_class', 'transferred_class')) and students.franchisee_id='".$franchiseeId."'")
                                    );
     return $students;
 	}
@@ -46,7 +46,7 @@ class StudentClasses extends \Eloquent {
                 $students=DB::select(DB::raw(
                         "SELECT * from students where id NOT IN (SELECT distinct(student_classes.student_id)
                          FROM student_classes where  enrollment_end_date >= '".$present_date->toDateString()."' AND student_classes.status 
-                         IN ('enrolled')) and students.franchisee_id= '".$franchiseeId."'")
+                         IN ('enrolled', 'transferred_to_other_class', 'transferred_class')) and students.franchisee_id= '".$franchiseeId."'")
                                    );
                 
                 /*
